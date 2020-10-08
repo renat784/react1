a811358bfc75d9fa59cf6044e170dde23db43fd2
Renat S <contact.renat@gmail.com>
2020-10-08 16:16:02 +0300

    Updates

---
+++ precache-manifest.95cf50b4b12d99dca609704a7de6ad95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13c225c489044402e5a2e24022132ee7",
+    "url": "/React-Deploy/index.html"
+  },
+  {
+    "revision": "afb9ee7f8234a62f5a1a",
+    "url": "/React-Deploy/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "30e86be970f509a1de90",
+    "url": "/React-Deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "afb9ee7f8234a62f5a1a",
+    "url": "/React-Deploy/static/js/2.16fed61f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Deploy/static/js/2.16fed61f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30e86be970f509a1de90",
+    "url": "/React-Deploy/static/js/main.e6d85362.chunk.js"
+  },
+  {
+    "revision": "0cae9dcc9706fefc6e09",
+    "url": "/React-Deploy/static/js/runtime-main.1fb6a18f.js"
+  }
+]);
